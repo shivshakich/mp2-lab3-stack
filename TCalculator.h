91df0584e8b0eee4b370e290e1235ece38209d67
VlaShi <moyoMylo>
2023-12-22 17:22:25 +0300

    Написан TCalculator.h, требуются тесты для него

--- TCalculator.h
+++ TCalculator.h
@@ -2,6 +2,8 @@
 
 #include "TStack.h"
 
+// считаем, что infix не должен содеражать пробелов
+
 using std::string;
 
 static int Priority(char op) {
@@ -30,17 +32,84 @@ class TCalculator {
 	void ToPostfix(void);											// найти postfix
 public:
 	// конструктор
-	TCalculator(const string& in) : infix("(" + in + ")"), postfix(), operation(in.size() + 2), number(in.size() + 2) {}
+	TCalculator(const string& in) : infix(in), postfix(), operation(in.size()), number(in.size()) {}
 
-	int IsCorrect() const;												// проверка корректности this->infix
+	//int IsCorrect() const;												// проверка корректности this->infix
 
-	string GetInfix() const { return infix[1:infix.size() - 2]; }		// выдать значение infix
+	string GetInfix() const { return infix; }							// выдать значение infix
 	string GetPostfix() const { return postfix; }						// выдать значение postfix
 
 	double Calc(void);													// вычислить postfix
 };
 
 template <class T>
-int TCalculator<T>::IsCorrect() const {
+void TCalculator<T>::ToPostfix(void) {
+	ClearStacks();
+	postfix = "";
 
+	string str = '(' + infix + ')';
+	int lengthStr = str.length();
+
+	for (int i = 0; i < lengthStr; ++i) {
+		int prior = Priority(str[i]);
+
+		if (str[i] == '(')								// когда '('
+			operation.Push('(');
+		else if (str[i] == ')') {						// когда ')'
+			char c;
+			while ((c = operation.Pop()) != '(')		
+				postfix += c;
+		}
+		else if (prior > 0) {							// когда операция 
+			char c;
+			while (prior <= Priority(c = operation.Top())) {
+				postfix += c;
+				operation.Pop();
+			}
+		}
+		else {											// когда операнд
+			double n = std::strtod(str.substr(i));
+			string syr = string(n);
+			i += syr.length() - 1;
+			postfix += string(n);
+		}
+	}
 }
+
+template <class T> 
+double TCalculator<T>::Calc(void) {
+	ClearStacks();
+	ToPostfix();
+
+	int lengthPostfix = postfix.length();
+
+	for (int i = 0; i < lengthPostfix; ++i) {
+		char c = postfix[i];
+
+		if (Priority(c) > 0) {		// когда операция
+			double x1, x2;
+			x2 = number.Pop();
+			x1 = number.Pop();
+
+			switch (c) {
+			case '+': x1 = x1 + x2; break;
+			case '-': x1 = x1 - x2; break;
+			case '*': x1 = x1 * x2; break;
+			case '/': x1 = x1 / x2; break;
+			case '^': x1 = pow(x1,x2); break;
+			}
+
+			number.Push(x1);
+		}
+		else {						// когда операнд
+			double n = std::strtod(postfix.substr(i));
+			string syr = string(n);
+			i += syr.length() - 1;
+			number.Push(n);
+		}
+	}
+
+	return number.Push();
+}
+
+
